b8d055ed86b12aec44d08b6d01cdac8843231342
jeggorr <113131104+jeggorr@users.noreply.github.com>
2022-11-22 18:57:12 +0200

    Reactoring app from class based components to functional components

--- src/components/card-list/card-list.component.jsx
+++ src/components/card-list/card-list.component.jsx
@@ -1,19 +1,13 @@
-import { Component } from 'react';
 import CardContainer from '../card-container/card-container.component';
 
 import './card-list.styles.css';
 
-class CardList extends Component {
-  render() {
-    const { monsters } = this.props;
-    return (
-      <div className='card-list'>
-        {monsters.map((monster) => {
-          return <CardContainer monster={monster} />;
-        })}
-      </div>
-    );
-  }
-}
+const CardList = ({ monsters }) => (
+  <div className='card-list'>
+    {monsters.map((monster) => {
+      return <CardContainer monster={monster} />;
+    })}
+  </div>
+);
 
 export default CardList;
